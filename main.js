ad5db772605f7aa2e0764e4d090db828b0996ac5
Shuhrat <shuhratjalolov2022@gmail.com>
2022-12-26 07:36:47 +0300

    js uy iwi

---
+++ main.js
@@ -0,0 +1,25 @@
+var gName = prompt('Ismingizni kiriting')
+alert('Salom ' + gName + '. Sizga matematik misollar beramiz togri javob berishga harakat qiling.')
+var pr = prompt('6 + 3 = ?');
+var pr1 = prompt('6 * 3 = ?');
+var pr2 = prompt('6 / 3 = ?');
+var pr3 = prompt('6 - 3 = ?');
+var pr4 = prompt('8 - 5 = ?');
+var pr5 = prompt('15 + 25 = ?');
+var pr6 = prompt('15 % 3 = ?');
+var pr7 = prompt('25 * 3 = ?');
+var pr8 = prompt('30 / 3 = ?');
+var pr9 = prompt('15 / 3 = ?');
+var pr10 = prompt('44 - 22 = ?');
+alert(gName + 'javobingiz uchun rahmat. Javobni korish uchun kansolni oching')
+console.log('Sizning javobingiz ' + pr + '. Togri javob 6 + 3 = ' + (6 + 3));
+console.log('Sizning javobingiz ' + pr1 + '. Togri javob 6 * 3 = ' + (6 * 3));
+console.log('Sizning javobingiz ' + pr2 + '. Togri javob 6 / 3 = ' + (6 / 3));
+console.log('Sizning javobingiz ' + pr3 + '. Togri javob 6 - 3 = ' + (6 - 3));
+console.log('Sizning javobingiz ' + pr4 + '. Togri javob 8 - 5 = ' + (8 - 5));
+console.log('Sizning javobingiz ' + pr5 + '. Togri javob 15 + 25 = ' + (15 + 25));
+console.log('Sizning javobingiz ' + pr6 + '. Togri javob 15 % 3 = ' + (15 % 3));
+console.log('Sizning javobingiz ' + pr7 + '. Togri javob 25 * 3 = ' + (25 * 3));
+console.log('Sizning javobingiz ' + pr8 + '. Togri javob 30 / 3 = ' + (30 / 3));
+console.log('Sizning javobingiz ' + pr9 + '. Togri javob 15 / 3 = ' + (15 / 3));
+console.log('Sizning javobingiz ' + pr10 + '. Togri javob 44 - 22 = ' + (44 - 22));
